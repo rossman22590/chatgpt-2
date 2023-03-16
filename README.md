2bbd48bd71a3adbee106301087817d25148d81b3
umarhadi <hi@umarhadi.dev>
2023-03-17 00:49:24 +0700

    very first commit as always
    Signed-off-by: umarhadi <hi@umarhadi.dev>

---
+++ README.md
@@ -0,0 +1,8 @@
+# chatgpt
+
+very functional chatgpt clone
+
+## tech
+- nextjs
+- typescript
+- tailwindcss
---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }: AppProps<{}>) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
+}
