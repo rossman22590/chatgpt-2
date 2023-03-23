2776faece042a4e9cee8d6cfa4d50af2ff22d9c0
umarhadi <hi@umarhadi.dev>
2023-03-23 14:53:25 +0700

    feat: Add download functionality to CodeBlock component
    - Added download button to CodeBlock component
    - Implemented downloadAsFile function to download code block as a file with suggested file name based on language
    - Created programmingLanguages object in data.ts to map file extensions to programming languages
    - Created generateRandomString function in data.ts to generate random strings for suggested file names

--- components/Markdown/CodeBlock.tsx
+++ components/Markdown/CodeBlock.tsx
@@ -1,6 +1,8 @@
 import { FC, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark, oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { IconDownload } from '@tabler/icons-react';
+import { generateRandomString, programmingLanguages } from '@/utils/app/data';
 
 interface Props {
     language: string;
@@ -20,22 +22,50 @@ export const CodeBlock: FC<Props> = ({ language, value, lightMode }) => {
             }, 2000);
         });
     };
+    const downloadAsFile = () => {
+        const fileExtension = programmingLanguages[language] || '.file';
+        const suggestedFileName = `file-${generateRandomString(3, true)}${fileExtension}`;
+        const fileName = window.prompt('Enter file name', suggestedFileName);
 
+        if (!fileName) {
+            // user pressed cancel on prompt
+            return;
+        }
+
+        const blob = new Blob([value], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = fileName;
+        link.href = url;
+        link.style.display = "none";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+    };
     return (
-        <div className="relative text-[16px] pt-2">
+        <div className="relative text-[16px]">
+            <div className="flex items-center justify-end">
+                <button
+                    className="text-white bg-none py-0.5 px-2 rounded focus:outline-none hover:bg-blue-700 text-xs"
+                    onClick={copyToClipboard}
+                >
+                    {buttonText}
+                </button>
+                <button
+                    className="text-white bg-none py-0.5 px-2 rounded focus:outline-none hover:bg-blue-700 text-xs"
+                    onClick={downloadAsFile}
+                >
+                    <IconDownload size={16} />
+                </button>
+            </div>
+
             <SyntaxHighlighter
                 language={language}
                 style={lightMode === "light" ? oneLight : oneDark}
             >
                 {value}
             </SyntaxHighlighter>
-
-            <button
-                className="absolute top-[-8px] right-[0px] text-white bg-none py-0.5 px-2 rounded focus:outline-none hover:bg-blue-700 text-xs"
-                onClick={copyToClipboard}
-            >
-                {buttonText}
-            </button>
         </div>
     );
 };
